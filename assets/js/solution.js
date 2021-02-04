a477a216f4cb3c342ecd1375350baaeebc058d6c
Vitaliy-Gaitkulov <iri.vert33@gmail.com>
2021-02-04 23:40:15 +0200

    Complete User Card

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -1,13 +1,15 @@
-'use strict'
+"use strict";
 //new URL('https://www.facebook.com/DwayneJohnson'); // {hostname}
 
-const supportedSocialNetworks = new Map().set('www.facebook.com', 'fa fa-facebook')
-                                        .set('twitter.com', 'fa fa-twitter')
-                                        .set('www.instagramm.com', 'fa fa-instagramm');
+const supportedSocialNetworks = new Map()
+  .set("www.facebook.com", "fa fa-facebook")
+  .set("twitter.com", "fa fa-twitter")
+  .set("www.instagramm.com", "fa fa-instagramm");
 // key - hostname
 
-const lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam numquam facere deleniti optio non voluptatem. Autem voluptatum perspiciatis voluptas non.';
-const cardContainer = document.getElementById('root'); // ul
+const lorem =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam numquam facere deleniti optio non voluptatem. Autem voluptatum perspiciatis voluptas non.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam numquam facere deleniti optio non voluptatem. Autem voluptatum perspiciatis voluptas non.";
+const cardContainer = document.getElementById("root"); // ul
 
 const cards = responseData.map((place) => createPlaceCardElement(place)); // создаем li
 
@@ -21,58 +23,66 @@ cardContainer.append(...cards); // добавляем li в ul
 function createPlaceCardElement(place) {
   const { firstName, lastName, contacts } = place;
 
-  const p = createElement('p', { classNames: ['cardDescription'] }, [
-    document.createTextNode(lorem || ''),
+  const p = createElement("p", { classNames: ["cardDescription"] }, [
+    document.createTextNode(lorem || ""),
   ]);
 
-  const h2 = createElement('h2', { classNames: ['cardName'] }, [
-    document.createTextNode(firstName + ' ' + lastName),
+  const h2 = createElement("h2", { classNames: ["cardName"] }, [
+    document.createTextNode(firstName + " " + lastName),
   ]);
 
   const img = createCardImage(place);
 
-  const a = createUlElement(contacts);
+  const ulListElement = createUlElement(contacts);
 
-  const article = createElement('article', { classNames: ['cardContainer'] }, [
+  const article = createElement("article", { classNames: ["cardContainer"] }, [
     img,
     h2,
     p,
-    ...a
+    ulListElement,
   ]);
 
-  const wrapper = createElement('li', { classNames: ['cardWrapper'] }, [
+  const wrapper = createElement("li", { classNames: ["cardWrapper"] }, [
     article,
   ]);
 
   return wrapper; //htmllielement
 }
 
+function createUlElement(contacts) {
+  const ulList = document.createElement("ul");
+  ulList.classList.add("ulLinks");
 
-function createUlElement(contacts){
-    return contacts.map((contact) => {
-        const {hostname} = new URL(contact);
+  const li = contacts.map((contact) => {
+    const { hostname } = new URL(contact);
 
-        const cssClasses = supportedSocialNetworks.get(hostname);
-        const ulList = createElement('ul');
-        ulList.classList.add('ulLinks');
-        const a = document.createElement('a');
-        a.setAttribute('href', contact);
-        a.setAttribute('class', cssClasses);
-        return a;
-    })
+    const cssClasses = supportedSocialNetworks.get(hostname);
+    const a = document.createElement("a");
+    a.setAttribute("href", contact);
+    console.log(supportedSocialNetworks);
+    if(cssClasses === undefined){
+      a.setAttribute("class", 'fa fa-google');
+    }else{
+      a.setAttribute("class", cssClasses);
+    }
+    
+    return a;
+  });
+  ulList.append(...li);
+  return ulList;
 }
 
 function createCardImage(place) {
   const { firstName, lastName, id } = place;
 
-  const imageWrapper = document.createElement('div');
-  imageWrapper.setAttribute('id', `wrapper${id}`); // устанавливаем  id для контейнер картинки
-  imageWrapper.classList.add('imageWrapper');
+  const imageWrapper = document.createElement("div");
+  imageWrapper.setAttribute("id", `wrapper${id}`); // устанавливаем  id для контейнер картинки
+  imageWrapper.classList.add("imageWrapper");
   imageWrapper.style.backgroundColor = stringToColour(firstName);
 
-  const initials = document.createElement('div');
-  initials.classList.add('imagePlaceholder', 'imagePlacement');
-  initials.append(document.createTextNode(firstName[0] || ''));
+  const initials = document.createElement("div");
+  initials.classList.add("imagePlaceholder", "imagePlacement");
+  initials.append(document.createTextNode(firstName[0] || ""));
 
   createImage(place);
 
@@ -81,13 +91,13 @@ function createCardImage(place) {
 }
 
 function createImage({ profilePicture, firstName, id }) {
-  const img = document.createElement('img'); // = new Image();
-  img.setAttribute('src', profilePicture);
-  img.setAttribute('alt', firstName);
+  const img = document.createElement("img"); // = new Image();
+  img.setAttribute("src", profilePicture);
+  img.setAttribute("alt", firstName);
   img.dataset.id = id; // даём картинки её id
-  img.classList.add('cardImage', 'imagePlacement');
-  img.addEventListener('error', imageErrorHandler);
-  img.addEventListener('load', imageLoadHandler);
+  img.classList.add("cardImage", "imagePlacement");
+  img.addEventListener("error", imageErrorHandler);
+  img.addEventListener("load", imageLoadHandler);
 }
 
 /* 
@@ -116,10 +126,10 @@ function stringToColour(str) {
   for (let i = 0; i < str.length; i++) {
     hash = str.charCodeAt(i) + ((hash << 5) - hash);
   }
-  let colour = '#';
+  let colour = "#";
   for (let i = 0; i < 3; i++) {
     let value = (hash >> (i * 8)) & 0xff;
-    colour += ('00' + value.toString(16)).substr(-2);
+    colour += ("00" + value.toString(16)).substr(-2);
   }
   return colour;
 }
@@ -139,7 +149,6 @@ function stringToColour(str) {
  * @param {HTMLElement[]} children
  */
 function createElement(type, { classNames, onClick, attributes }, children) {
-
   const elem = document.createElement(type);
   elem.classList.add(...classNames);
   elem.onclick = onClick;
